25f286bae3da951d7a9d5c0de895a27b419fbeba
William Lima <willguitaradmfar@gmail.com>
2013-08-09 11:06:22 -0300

    Update server.js
    Ajuste de Env Port

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
-
   var fs = require('fs'),
     path = require('path'),
     sio = require('socket.io'),
     static = require('node-static');
 
   var app = require('http').createServer(handler);
-  app.listen(8000);
+  app.listen(process.env.PORT);
 
   var file = new static.Server(path.join(__dirname, '..', 'public'));
 
